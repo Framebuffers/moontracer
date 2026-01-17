37208792e3b328d1c4e883aa48d06fc52911a896
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2026-01-17 16:09:41 -0300

    feat: added new responses for discord interactions endpoint

--- src/app/api/v1/discord/interactions/route.ts
+++ src/app/api/v1/discord/interactions/route.ts
@@ -1,8 +1,29 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    const body = await request.json();
-    return NextResponse.json({
-        type: 1
-    });
+    try {
+        const contentType = request.headers.get('content-type');
+
+        if (!contentType?.includes('application-json')) {
+            return NextResponse.json({
+                error: 'Content type must be application/json'
+            }, { status: 400 })
+        }
+
+        const body = await request.json();
+
+        if (body.type === 1) {
+            return NextResponse.json({ type: 1 });
+        }
+
+        if (body.type === 4) {
+            return NextResponse.json({
+                type: 4,
+                data: { content: 'Received interaction.' }
+            });
+        }
+    } catch(error) {
+        console.log('Error: ', error);
+        return NextResponse.json({error: 'Failed to process'}, {status: 500});
+    }
 }
