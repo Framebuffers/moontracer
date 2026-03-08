01d16d30c66497c78c04ed378ecea0c5f2387bbf
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2026-03-07 23:04:49 -0300

    ADD: player options

--- internal/commands/add_player.go
+++ internal/commands/add_player.go
@@ -11,6 +11,28 @@ type addPlayer struct {
 	db *bun.DB
 }
 
+/*
+
+	One of the issues I've been having, is that every single time someone gets added to a campaign,
+	the bot we *currently* use (yagpdb.xyz) pings **every single member** of the campaign.
+
+	And I don't wanna have that happening, it is annoying af.
+
+	This is the plan:
+		- A custom type: Notification
+		- Notifications have scopes: Member, DM, Mod, Owner.
+		- They have an array attached to which members to notify, an empty array means every member of that category.
+		- It whitelists by default, starting with the most restrictive access, moving up.
+		- Default is DM only. If Players are added, scopes broaden, adding them to each respecitve category.
+		- If you want to ping a group, pass the scope and an array, empty if all members are notified, else it notifies the ones inside.
+
+		Checks:
+		- Player has roles (registered player, registered mod, admin, mod, etc.)
+		- Player is inside Campaign
+		- Player has DMs enabled
+
+*/
+
 // Data is the command metadata that Discord shows to users.
 func (r *addPlayer) Data() *discordgo.ApplicationCommand {
 	return &discordgo.ApplicationCommand{
