66677c955576f7be1d92e9f1c420ffffa6a54c2e
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2026-05-10 01:58:11 -0400

    FIX: UX changes, third row for setting a cover and links buttons

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -13,7 +13,7 @@ package messages
 const (
 	GenericErrorMessage      = "Something went wrong."
 	InvalidButtonDataMessage = "Invalid button data."
-	BotVersion               = "v0.11.10"
+	BotVersion               = "v0.11.11"
 )
 
 /*
