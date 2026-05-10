f568d5fb5868f5f2a58694e0ac9a6367aa16da2e
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2026-05-10 01:44:22 -0400

    BUMP v.0.11.9

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -13,7 +13,7 @@ package messages
 const (
 	GenericErrorMessage      = "Something went wrong."
 	InvalidButtonDataMessage = "Invalid button data."
-	BotVersion               = "v0.11.0"
+	BotVersion               = "v0.11.9"
 )
 
 /*
