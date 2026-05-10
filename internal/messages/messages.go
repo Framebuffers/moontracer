77a7ad0cbc2c85e651aa53ae9f4b343579bfcd73
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2026-05-10 01:51:52 -0400

    FIX: banned members do not appear on campaign cards

--- internal/messages/messages.go
+++ internal/messages/messages.go
@@ -13,7 +13,7 @@ package messages
 const (
 	GenericErrorMessage      = "Something went wrong."
 	InvalidButtonDataMessage = "Invalid button data."
-	BotVersion               = "v0.11.9"
+	BotVersion               = "v0.11.10"
 )
 
 /*
